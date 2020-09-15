3f37a17221c1fd88b84751eb7d6afd5347cc00de
marcel1099 <m_st97@users.noreply.github.com>
2020-09-15 11:14:02 +0200

    Update LED.ino

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,9 +1,19 @@
+#define grueneLED 3
+
 void setup() {
-  // put your setup code here, to run once:
+  digitalWrite (grueneLED, OUTPUT)
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  digitalWrite(grueneLED, HIGH)
+  delay(1000)
+  digitalWrite(grueneLED, LOW)
+  delay(1000)
+  digitalWrite(grueneLED, HIGH)
+  delay(1000)
+  digitalWrite(grueneLED, LOW)
+  delay(1000)
 }
+
